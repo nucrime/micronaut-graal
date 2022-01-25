609b2eff9663a6ded02312638543b7d34d5dd9b9
Alexander Karankevich <givmemp3@gmail.com>
2022-01-25 22:35:01 +0300

    init

---
+++ src/test/java/by/ak/MicronautGraalTest.java
@@ -0,0 +1,21 @@
+package by.ak;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronautGraalTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
